403784cf6be479929b176bb01f16451b1d47fb8b
iamgriffon <guusilveira@gmail.com>
2023-01-19 20:49:03 -0300

    finally fixed monorepo structure!

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,44 +1,12 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_800ExtraBold
-} from '@expo-google-fonts/inter';
-import { Loading } from './src/components/loading';
+import { Text, View  } from 'react-native';
 
 export default function App() {
-
-  const [fonstLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_600SemiBold,
-    Inter_700Bold,
-    Inter_800ExtraBold
-  });
-
-  if(!fonstLoaded) {
-    return <Loading />
-  }
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="light" backgroundColor='transparent' translucent />
+    <View className='bg-background h-full w-100'>
+      <Text className='text-black'>CU</Text>
+      <StatusBar style='light' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'red',
-    fontFamily: "Inter_800ExtraBold"
-  }
-});
--- packages/mobile/src/components/loading.tsx
+++ packages/mobile/src/components/loading.tsx
@@ -1,10 +1,10 @@
 import { ActivityIndicator, View } from "react-native";
+import React from 'react'
 
 export function Loading(){
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <ActivityIndicator color="#7C3ACD"/>
-
+    <View className="bg-black">
+      <ActivityIndicator/>
     </View>
   )
 }
